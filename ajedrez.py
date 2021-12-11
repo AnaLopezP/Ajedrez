fcfddb6a6648a8b982eecf2b6708897014ef4836
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-11 18:30:23 +0100

    funcion guardar tablero

--- ajedrez.py
+++ ajedrez.py
@@ -7,7 +7,11 @@ tablero = [['TN1', 'CN1', 'AN1', 'RN', 'RYN', 'AN2', 'CN2', 'TN2' ],
            ['PB1', 'PB2', 'PB3', 'PB4', 'PB5', 'PB6', 'PB7', 'PB8'],
            ['TB1', 'CB1', 'AB1', 'RB', 'RYB', 'AB2', 'CB2', 'TB2',]]
 
+def guardar_tablero(lista, file):
+    file = open(file, "w")
+    file.write(str(lista))
+    file.close
 
-file = open("prueba.txt", "w")
-file.write("prueba 1\n")
-file.write("prueba 2")
+print("¿Como quieres que se llame el fichero?")
+fichero = input()
+guardar_tablero(tablero, fichero)
