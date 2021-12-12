3c74acda42681f7ff50d2493ae873c1f7d3ef7a4
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-12 13:35:36 +0100

    primera tarea terminada

--- ajedrez.py
+++ ajedrez.py
@@ -7,24 +7,29 @@ tablero = [['TN1', 'CN1', 'AN1', 'RN', 'RYN', 'AN2', 'CN2', 'TN2' ],
            ['PB1', 'PB2', 'PB3', 'PB4', 'PB5', 'PB6', 'PB7', 'PB8'],
            ['TB1', 'CB1', 'AB1', 'RB', 'RYB', 'AB2', 'CB2', 'TB2',]]
 
-def guardar_tablero(lista, file):
-    file = open(file + '.txt', "w")
+contador = 0
+
+def guardar_tablero(lista, file, contador):
+    file = open(file + '.txt', "a")
+    file.write("Movimiento " + str(contador) + "\n")
     for i in range(0, 8):
         for j in range(0, 8):
             if j == 7:
                 file.write(str(lista[i][j]) + '\n')
             else:
                 file.write(str(lista[i][j]) + '\t')
-    file.close
+    
+    
 
 def mover_ficha(fila_ficha, columna_ficha, fila_casilla, columna_casilla):
     tablero[int(fila_casilla)][int(columna_casilla)] = tablero[int(fila_ficha)][int(columna_ficha)]
     tablero[int(fila_ficha)][int(columna_ficha)] = ' '
-    
-contador = 0
+
+
+
 print("¿Como quieres que se llame el fichero?")
 fichero = input()
-guardar_tablero(tablero, fichero)
+guardar_tablero(tablero, fichero, contador)
 print("tu archivo ha sido creado.\n¿Quieres mover o acabar la partida?")
 decision = input()
 while decision == 'mover':
@@ -36,8 +41,10 @@ while decision == 'mover':
     columna_casilla = input()
     mover_ficha(fila_ficha, columna_ficha, fila_casilla, columna_casilla)
     contador = contador + 1
-    movimiento = 'movimiento' + str(contador)
-    guardar_tablero(tablero, movimiento)
-    print("¿Qué quiere hacer ahora")
+    guardar_tablero(tablero, fichero, contador)
+    print("¿Qué quiere hacer ahora?")
     decision = input()
 print("Tu partida ha acabado, gracias por jugar.")
+
+print("¿Qué movimiento quieres ver en pantalla?")
+
